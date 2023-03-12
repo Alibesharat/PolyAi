fdf80cea664484113561904a4fc23733a2b47d2d
Ali besharati <devs.ali.b@gmail.com>
2023-03-12 20:50:12 +0800

    regisyter

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using Primal_e.Data;
+using Primal_e.Data.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddSingleton<UserService>();
 
 var app = builder.Build();
 
